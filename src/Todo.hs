c92269bd27a01953c323625a9f3c6a19940983b4
Sebastian Claus <clausseb@hu-berlin.de>
2020-08-12 21:28:13 +0200

    feat(Todo): add parsing via `Read Todo` instance

--- src/Todo.hs
+++ src/Todo.hs
@@ -1,8 +1,93 @@
-module Todo where
+module Todo (Todo, isDone, getDescription) where
 
-import Text.Read
+-- import Text.Read
+-- import Text.ParserCombinators.ReadPrec
+import Data.Time (fromGregorian, Day)
+import Control.Applicative ((<*>),(<$>))
+import Text.ParserCombinators.ReadP
+import Control.Monad (replicateM, forM)
+import Text.ParserCombinators.ReadP (count)
+import Data.Char (isSpace)
 
-data Todo = Todo
+
+-- TODO Tag parsing
+
+data Todo =
+    Todo { isDone            ::       Bool
+         , getPriority       :: Maybe String
+         , getCompletionDate :: Maybe Day
+         , getCreationDate   :: Maybe Day
+         , getDescription    ::       String
+    } deriving (Show)
 
 instance Read Todo where
-    readPrec = undefined
+    readsPrec _ s = result
+        where result = 
+                case readP_to_S parseTodo s of
+                    [] -> []
+                    [x] -> [x]
+                    x:_ -> [x] -- NOTE / FIXME this seems to be quite a crude fix to the ambiguous parsing
+
+
+-- instance Show Todo where
+--     show = undefined
+
+
+
+{-----------------------
+    PARSING HELPER
+-------------------------}
+
+parseTodo :: ReadP Todo
+parseTodo = do
+    done <- optDone
+    prio <- optPriority
+    complDate <- optCompletionDate
+    createDate <- optCreationDate
+    desc <- description
+    return $ Todo done prio complDate createDate desc
+
+
+
+digit :: ReadP Char
+digit = choice $ map char ['0' .. '9']
+
+rest :: ReadP [Char]
+rest = length <$> look >>= \n -> count n get
+
+maybeParse :: ReadP a -> ReadP (Maybe a)
+maybeParse p = option Nothing $ Just <$> p
+
+-- parseDate :: ReadPrec Day
+parseDate :: ReadP Day
+parseDate = do
+    year <- read <$> count 4 digit
+    char '-'
+    month <- read <$> count 2 digit
+    char '-'
+    day <- read <$> count 2 digit
+
+    return $ fromGregorian year month day
+
+
+
+optDone :: ReadP Bool
+optDone = option False $ trueP <* munch1 isSpace
+    where   trueP = char 'x' >> return True
+
+
+optPriority :: ReadP (Maybe String)
+optPriority = maybeParse aPrio
+    where   aPrio  = between (char '(')  (char ')') (munch1 $ (/= ')'))  <* skipSpaces
+
+
+optCompletionDate :: ReadP (Maybe Day)
+optCompletionDate = maybeParse parseDate'
+    where parseDate' = parseDate <* skipSpaces
+
+optCreationDate :: ReadP (Maybe Day)
+optCreationDate = maybeParse parseDate'
+    where parseDate' = parseDate <* skipSpaces
+
+description :: ReadP String
+description = rest
--- test/TodoSpec.hs
+++ test/TodoSpec.hs
@@ -6,8 +6,38 @@ import Todo
 
 spec = do
     describe "Parsing according to todotxt.org" $ do
-        context "when all fields are set (completed, priority \
-                \ completion date, creation date, description, but NO tags" $ do
+        context "when just a description is set" $ do
+            it "parses and is an undone description" $
+                let
+                    sut = read "Review Tim's pull request" :: Todo
+                in do
+                    sut `shouldSatisfy` (not.isDone)
+                    getDescription sut `shouldBe` "Review Tim's pull request"
+
+            it "parses and is an   done description" $
+                let
+                    sut = read "x Review Tim's pull request" :: Todo
+                in do
+                    sut `shouldSatisfy` isDone
+                    getDescription sut `shouldBe` "Review Tim's pull request"
+
+            it "is only description despite trailing x" $
+                let
+                    sut = read "xReview Tim's pull request" :: Todo
+                in do
+                    sut `shouldSatisfy` (not.isDone)
+                    getDescription sut `shouldBe` "xReview Tim's pull request"
+
+        context "with creation/deletiong date" $ do
+            it "..."
+                pending
+
+        context "when all fields are set but NO tags" $ do
             it "parses succesfully" $
-                let sut = read "x 2011-03-02 2011-03-01 Review Tim's pull request" :: Todo
-                in pending
+                let sut = read "x (A) 2011-03-02 2011-03-01 Review Tim's pull request" :: Todo
+                in do
+                    sut `shouldSatisfy` isDone
+                    -- getCompletionDate sut `shouldBe` ...
+                    -- getCreationDate sut
+                    -- getDescription
+                    -- getPriority
