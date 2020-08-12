9a63b14af3e585010fa76fa442aaabd15bcd3178
Sebastian Claus <clausseb@hu-berlin.de>
2020-08-12 22:51:47 +0200

    test(Todo): finished test case when all fields are set

--- src/Todo.hs
+++ src/Todo.hs
@@ -1,38 +1,45 @@
-module Todo (Todo, isDone, getDescription) where
+module Todo
+  ( Todo,
+    isDone,
+    getDescription,
+    getCompletionDate,
+    getPriority,
+    getCreationDate,
+  )
+where
 
 -- import Text.Read
 -- import Text.ParserCombinators.ReadPrec
-import Data.Time (fromGregorian, Day)
-import Control.Applicative ((<*>),(<$>))
-import Text.ParserCombinators.ReadP
-import Control.Monad (replicateM, forM)
-import Text.ParserCombinators.ReadP (count)
-import Data.Char (isSpace)
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (forM, replicateM)
+import Data.Char (isSpace)
+import Data.Time (Day, fromGregorian)
+import Text.ParserCombinators.ReadP
+import Text.ParserCombinators.ReadP (count)
 
 -- TODO Tag parsing
 
-data Todo =
-    Todo { isDone            ::       Bool
-         , getPriority       :: Maybe String
-         , getCompletionDate :: Maybe Day
-         , getCreationDate   :: Maybe Day
-         , getDescription    ::       String
-    } deriving (Show)
+data Todo = Todo
+  { isDone :: Bool,
+    getPriority :: Maybe String,
+    getCompletionDate :: Maybe Day,
+    getCreationDate :: Maybe Day,
+    getDescription :: String
+  }
+  deriving (Show)
 
 instance Read Todo where
-    readsPrec _ s = result
-        where result = 
-                case readP_to_S parseTodo s of
-                    [] -> []
-                    [x] -> [x]
-                    x:_ -> [x] -- NOTE / FIXME this seems to be quite a crude fix to the ambiguous parsing
-
+  readsPrec _ s = result
+    where
+      result =
+        case readP_to_S parseTodo s of
+          [] -> []
+          [x] -> [x]
+          x : _ -> [x] -- NOTE / FIXME this seems to be quite a crude fix to the ambiguous parsing
 
 -- instance Show Todo where
 --     show = undefined
-
-
 
 {-----------------------
     PARSING HELPER
@@ -40,14 +47,12 @@ instance Read Todo where
 
 parseTodo :: ReadP Todo
 parseTodo = do
-    done <- optDone
-    prio <- optPriority
-    complDate <- optCompletionDate
-    createDate <- optCreationDate
-    desc <- description
-    return $ Todo done prio complDate createDate desc
-
-
+  done <- optDone
+  prio <- optPriority
+  complDate <- optCompletionDate
+  createDate <- optCreationDate
+  desc <- description
+  return $ Todo done prio complDate createDate desc
 
 digit :: ReadP Char
 digit = choice $ map char ['0' .. '9']
@@ -61,33 +66,33 @@ maybeParse p = option Nothing $ Just <$> p
 -- parseDate :: ReadPrec Day
 parseDate :: ReadP Day
 parseDate = do
-    year <- read <$> count 4 digit
-    char '-'
-    month <- read <$> count 2 digit
-    char '-'
-    day <- read <$> count 2 digit
+  year <- read <$> count 4 digit
+  char '-'
+  month <- read <$> count 2 digit
+  char '-'
+  day <- read <$> count 2 digit
 
-    return $ fromGregorian year month day
-
-
+  return $ fromGregorian year month day
 
 optDone :: ReadP Bool
 optDone = option False $ trueP <* munch1 isSpace
-    where   trueP = char 'x' >> return True
-
+  where
+    trueP = char 'x' >> return True
 
 optPriority :: ReadP (Maybe String)
 optPriority = maybeParse aPrio
-    where   aPrio  = between (char '(')  (char ')') (munch1 $ (/= ')'))  <* skipSpaces
-
+  where
+    aPrio = between (char '(') (char ')') (munch1 $ (/= ')')) <* skipSpaces
 
 optCompletionDate :: ReadP (Maybe Day)
 optCompletionDate = maybeParse parseDate'
-    where parseDate' = parseDate <* skipSpaces
+  where
+    parseDate' = parseDate <* skipSpaces
 
 optCreationDate :: ReadP (Maybe Day)
 optCreationDate = maybeParse parseDate'
-    where parseDate' = parseDate <* skipSpaces
+  where
+    parseDate' = parseDate <* skipSpaces
 
 description :: ReadP String
 description = rest
